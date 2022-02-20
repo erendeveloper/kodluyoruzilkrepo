3fc4e7b234c64b0303e1daa601bded1a131a9015
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2022-02-20 04:12:17 +0300

    C# 101 - hata-yonetimi
    C# 101 - hata-yonetimi

---
+++ patika.dev/C# 101/hata-yonetimi/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace hata_yonetimi{
+    class Program{
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bir sayi girin");
+            int sayi = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sayi);
+
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Hata: "+ex.Message.ToString());
+            }
+            finally{
+                Console.WriteLine("Islem tamamlanmdi");
+            }
+
+            try
+            {
+               // int a = UInt16.Parse(null);
+                //int a = int.Parse("test");
+                int a = int.Parse("-20000000000");
+
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Bos deger girdiniz");
+                Console.WriteLine(ex);
+;           }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine("Veri tipi uygun degil");
+                Console.WriteLine(ex);
+;           }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("cok kucuk yada cok buyuk bir deger girsdiniz");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("Islem basariyla tamamlanmdi");
+            }
+
+        }
+    }
+
+}
