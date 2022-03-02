84af757372fd7eff3eb853b2ce2f7bfc616b5257
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2022-03-02 17:44:32 +0300

    if else, ternary
    if else, ternary

---
+++ patika.dev/C# 101/if-else if/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace if_else_if{
+    class Program{
+        static void Main(string[] args){
+            int time = DateTime.Now.Hour;
+            if(time>=6 && time>=1){
+               Console.WriteLine("gunaydin");
+            }
+            else if(time<=18){
+                Console.WriteLine("iyi gunler");
+            }
+            else{
+            Console.WriteLine("iyi geceler");
+            }
+
+            string sonuc = time<=18 ? "iyi gunler" : "iyi geceler";
+
+            sonuc = time>=6 && time<11 ? "iyi gunler" : time<=18 ? "iyi geceler" : "iyi geceler";
+
+            Console.WriteLine(sonuc);
+
+        }
+
+    }
+}
