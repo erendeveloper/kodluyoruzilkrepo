792911b0c62f0a672da78adf311edfbee7cbdd6a
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2022-03-06 09:19:33 +0300

    donguler-while-foreach
    donguler-while-foreach

---
+++ patika.dev/C# 101/donguler-while-foreach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace donguler_while_foreach{
+    class Program{
+        static void Main(string[] args)
+        {
+            //1 den baslayarak konsoldan girilen sayiya kadar sayi dahil ortalama hesapla
+            Console.WriteLine("sayi gir");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while(sayac<=sayi){
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a'dan z ye kadar tum harfleri konsola yazdir
+            char character ='a';
+            while(character < 'z'){
+                Console.Write(character);
+                character ++;
+            }
+
+            Console.WriteLine("*** foreach ***");
+            string[] arabalar = {"bmw","ford","nissan"};
+            foreach(var araba in arabalar){
+                Console.WriteLine(araba);
+            }
+        }
+    }
+
+}
