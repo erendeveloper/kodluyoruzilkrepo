5408b2713ee7a00d957457e73c6a9b84d5d132d8
erendeveloper <48724906+erendeveloper@users.noreply.github.com>
2022-03-06 08:16:26 +0300

    switch-case
    switch-case

---
+++ patika.dev/C# 101/switch-case/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace swwitch_case{
+    class Program{
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            //expression
+            switch (month)
+            {
+                case 1:
+                   Console.WriteLine("Ocak ayindasiniz");
+                   break;
+                case 2:
+                   Console.WriteLine("Subat ayindasiniz");
+                   break;
+
+                case 4:
+                   Console.WriteLine("Nisan ayindasiniz");
+                   break;
+                
+                case 3:
+                   Console.WriteLine("Mart ayindasiniz");
+                   break;
+
+                default:
+                   Console.WriteLine("Yanlis veri girdiniz");
+                   break;
+            }
+        }
+    }
+
+}
